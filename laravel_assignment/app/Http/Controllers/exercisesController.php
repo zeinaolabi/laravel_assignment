821ae55f0aaa84797df29788944425e767366e97
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-30 14:58:10 +0300

    implement sort string algo

--- laravel_assignment/app/Http/Controllers/exercisesController.php
+++ laravel_assignment/app/Http/Controllers/exercisesController.php
@@ -11,6 +11,7 @@ class exercisesController extends Controller
         $lowercaseString = [];
         $uppercaseString = [];
         $numString = [];
+        $sortedString = [];
         $stringParts = str_split($string);
         for($i = 0; $i < count($stringParts); $i++){
             if(IntlChar::islower($stringParts[$i])){
@@ -24,10 +25,46 @@ class exercisesController extends Controller
             }
         }
         sort($lowercaseString);
-        echo implode($lowercaseString);
         sort($uppercaseString);
-        echo implode($uppercaseString);
         sort($numString);
-        echo implode($numString);
+
+        $sortedNums = implode($numString);
+
+        for($i = 0; $i < count($lowercaseString); $i++){
+            for($j = 0; $j < count($uppercaseString); $j++){
+                if(ord($lowercaseString[$i])%32 < ord($uppercaseString[$j]) %32){
+                    $sortedString[] = $lowercaseString[$i];
+                    array_splice($lowercaseString, $i, 1);
+                    break;
+                }
+                elseif(ord($lowercaseString[$i])%32 > ord($uppercaseString[$j]) %32){
+                    $sortedString[] = $uppercaseString[$j];
+                    array_splice($uppercaseString, $j, 1);
+                }
+                else{
+                    $sortedString[] = $lowercaseString[$i];
+                    if(($i + 1) < count($lowercaseString) && ord($lowercaseString[$i+1])%32 == ord($uppercaseString[$j]) %32){
+                        array_splice($lowercaseString, $i, 1);
+                        break;
+                    }
+                    $sortedString[] = $uppercaseString[$j];
+                    array_splice($lowercaseString, $i, 1);
+                    array_splice($uppercaseString, $j, 1);
+                    break;
+                }
+            }
+        }
+
+        for($i = 0; $i < count($lowercaseString); $i++){
+            $sortedString[] = $lowercaseString[$i];
+        }
+
+        for($i = 0; $i < count($uppercaseString); $i++){
+            $sortedString[] = $uppercaseString[$i];
+        }
+
+        echo implode($sortedString).$sortedNums;
+
+
     }
 }
